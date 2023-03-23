87f9a02cb42042fad72869335a5c95e709ea97d5
MikhailMikheev <Mikhail-Mikheev-7@yandex.ru>
2023-03-23 21:35:54 +0300

    добавил 3 задачу

---
+++ Seminar1.2/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.Clear();
+
+Console.WriteLine("Enter number of day of the week");
+int day = int.Parse(Console.ReadLine());
+
+if (day == 1)
+{
+  Console.WriteLine("Это понедельник");
+}
+
+if (day == 2)
+{
+  Console.WriteLine("Это вторник");
+}
+
+if (day == 3)
+{
+  Console.WriteLine("Это среда");
+}
+
+if (day == 4)
+{
+  Console.WriteLine("Это четверг");
+}
+
+if (day == 5)
+{
+  Console.WriteLine("Это пятница");
+}
+
+if (day == 6)
+{
+  Console.WriteLine("Это суббота");
+}
+
+if (day == 7)
+{
+  Console.WriteLine("Это воскресение");
+}
