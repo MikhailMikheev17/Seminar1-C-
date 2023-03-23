14770332848c87621bcf7d655dd8cc2c011a55c3
MikhailMikheev <Mikhail-Mikheev-7@yandex.ru>
2023-03-23 21:42:05 +0300

    Обновил третью задачу

--- Seminar1.2/Program.cs
+++ Seminar1.2/Program.cs
@@ -8,32 +8,36 @@ if (day == 1)
   Console.WriteLine("Это понедельник");
 }
 
-if (day == 2)
+else if (day == 2)
 {
   Console.WriteLine("Это вторник");
 }
 
-if (day == 3)
+else if (day == 3)
 {
   Console.WriteLine("Это среда");
 }
 
-if (day == 4)
+else if (day == 4)
 {
   Console.WriteLine("Это четверг");
 }
 
-if (day == 5)
+else if (day == 5)
 {
   Console.WriteLine("Это пятница");
 }
 
-if (day == 6)
+else if (day == 6)
 {
   Console.WriteLine("Это суббота");
 }
 
-if (day == 7)
+else if (day == 7)
 {
   Console.WriteLine("Это воскресение");
+}
+else 
+{
+  Console.WriteLine("Некорректный день недели");
 }
