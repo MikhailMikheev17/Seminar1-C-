c345b9788ead61183143c3fe23a2d60a7eee34df
MikhailMikheev <Mikhail-Mikheev-7@yandex.ru>
2023-03-27 20:38:11 +0300

    add Seminar 2.1

---
+++ Seminar2.1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+int num = new Random().Next(10,100);
+
+int num1 = num/10;
+int num2 = num%10;
+Console.WriteLine($"num = {num}");
+if (num1>num2) 
+{
+  Console.WriteLine($"{num1}>{num2} max = {num1}");
+}
+else if (num1<num2)
+{
+  Console.WriteLine($"{num1}<{num2} max = {num2}");
+}
+else 
+{
+  Console.WriteLine($"{num1}={num2}  max = {num1}");
+}
