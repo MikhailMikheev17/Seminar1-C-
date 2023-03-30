c3d9ba970578a4342e36f8068d2c2a6f39a6b762
MikhailMikheev <Mikhail-Mikheev-7@yandex.ru>
2023-03-30 20:23:59 +0300

    add Seminar 3.2

---
+++ Seminar3.2/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.Clear();
+
+System.Console.Write(" Enter number of sectioin :");
+int numSection = int.Parse(Console.ReadLine());
+
+if (numSection == 1)
+{
+  System.Console.WriteLine(" x > 0, y > 0");
+}
+else if (numSection == 2)
+{
+  System.Console.WriteLine("x < 0, y > 0");
+}
+else if (numSection == 3)
+{
+  System.Console.WriteLine("x < 0 , y < 0");
+}
+else if (numSection == 4)
+{
+  System.Console.WriteLine(" x > 0 , y < 0");
+}
+else System.Console.WriteLine("No section");
