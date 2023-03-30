0e8b699fbd3583c5f74c2f6bc9f22b457b4a07bf
MikhailMikheev <Mikhail-Mikheev-7@yandex.ru>
2023-03-30 21:41:02 +0300

    add seminar 3.5

---
+++ Seminar3.5/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел 
+//от 1 до N.
+
+//5 -> 1, 4, 9, 16, 25.
+//2 -> 1,4
+
+Console.Clear();
+
+System.Console.Write("Enter number N: ");
+int numN = int.Parse(Console.ReadLine());
+
+for (double i = 1; i <= numN;i++)
+{
+  double pow = Math.Pow(i,2);
+  System.Console.Write($"{pow} ");
+}
+
