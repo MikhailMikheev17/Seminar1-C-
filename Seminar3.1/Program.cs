6b01fde827e2a501b9dfc8f3c17be2856b8eb242
MikhailMikheev <Mikhail-Mikheev-7@yandex.ru>
2023-03-30 20:11:41 +0300

    add Seminar 3.1

---
+++ Seminar3.1/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y), 
+//причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+Console.Clear();
+
+System.Console.Write("Enter number x :");
+int numX = int.Parse(Console.ReadLine());
+
+System.Console.Write("Enter number y :");
+int numY = int.Parse(Console.ReadLine());
+
+if (numX > 0 && numY > 0)
+{
+  System.Console.WriteLine("Первая четверть");
+}
+else if( numX < 0 && numY > 0)
+{
+  System.Console.WriteLine("Вторая четверть ");
+}
+else if( numX < 0 && numY < 0)
+{
+  System.Console.WriteLine("Третья четверть ");
+}
+else 
+{
+  System.Console.WriteLine("Четвертая четверть ");
+}
+
