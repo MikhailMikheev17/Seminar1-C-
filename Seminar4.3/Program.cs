03852ae4b3d9147723b1a3f14d64de5cc6071136
MikhailMikheev <Mikhail-Mikheev-7@yandex.ru>
2023-04-05 12:57:56 +0300

    add seminar 4.3

---
+++ Seminar4.3/Program.cs
@@ -0,0 +1,28 @@
+﻿// от 1 до N умножение 
+
+Console.Clear();
+
+int SetNumber(string text = "")
+{
+  Console.Write($"Enter number {text}: ");
+  int num = Convert.ToInt32(Console.ReadLine());
+  return num;
+}
+
+int MethodMultiplication( int number)
+{
+  int i = 1;
+  int multi = 1;
+  for (;i<=number;i++)
+  {
+    multi = multi * i;
+  }
+  return multi;
+  //System.Console.WriteLine($" multiplication = {multi}");
+}
+
+int num = SetNumber();
+
+int multip = MethodMultiplication(num);
+System.Console.WriteLine($"multiplication = {multip}");
+
