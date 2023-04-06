0584c873fac9a07f66edb3b847b39733a56c3de6
MikhailMikheev <Mikhail-Mikheev-7@yandex.ru>
2023-04-06 21:29:58 +0300

    update seminat5.1

--- Seminar5.1/Program.cs
+++ Seminar5.1/Program.cs
@@ -32,3 +32,46 @@ int[] array = FillArray(12, -9, 9);
 
 System.Console.WriteLine(string.Join(", ",array));
 System.Console.WriteLine($"negativeSum = {negative}; positiveSum = {positive}");
+
+int size = 12;
+int[] nums = GetArray(size);
+Console.WriteLine(string.Join("; ", nums));
+
+int[] GetArray(int size)
+{
+    int[] result = new int [size];
+    Random rnd = new Random();
+    for(int i = 0; i < size; i++)
+        result[i] = rnd.Next(-9, 10);
+   
+    return result;
+}
+
+int SumNumPlus(int[] array)
+{
+int result = 0;
+for(int i = 0; i < size; i++)
+    {
+        if(array[i] > 0)
+        {
+            result = result + array[i];
+        }
+    }
+    return result;
+}
+
+int SumNumMin(int[] array)
+{
+int result = 0;
+for(int i = 0; i < size; i++)
+    {
+        if(array[i] < 0)
+        {
+            result = result + array[i];
+        }
+    }
+    return result;
+}
+
+Console.WriteLine($"Сумма положительных чисел: {SumNumPlus(nums)}");
+Console.WriteLine($"Сумма отрицательных чисел: {SumNumMin(nums)}");
