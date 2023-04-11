928735cd4867071a299dd8fb3dccc1232649b081
MikhailMikheev <Mikhail-Mikheev-7@yandex.ru>
2023-04-11 13:37:07 +0300

    add Seminar 6.3

---
+++ Seminar6.3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача40.Напишите программу, которая будет 
+// преобразовывать десятичное число в двоичное.
+
+Console.Clear();
+
+System.Console.WriteLine(" enter  number  >  ");
+int num  = int.Parse(Console.ReadLine());
+System.Console.WriteLine(" enter base  >  ");
+int @base  = int.Parse(Console.ReadLine());
+System.Console.WriteLine(ChangeBase(num,@base));
+
+string ChangeBase(int num, int @base)
+{
+  int[] array = new int[32];
+  for (int i = 31; ; i--)
+  {
+    array[i] = num % @base;
+    num /= @base;
+    if (num == 0) break;
+  }
+  return String.Join("", array);
+}
+
+
+
+
