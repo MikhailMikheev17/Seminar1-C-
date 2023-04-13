5b794fdc938ddd9b326b06131615fda76f7345f7
MikhailMikheev <Mikhail-Mikheev-7@yandex.ru>
2023-04-13 21:16:41 +0300

    add Semiar 7.2

---
+++ Seminar7.2/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, 
+// каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+Console.Clear();
+
+Start();
+
+void Start()
+{
+    int rows = SetNumber("rows");
+    int columns = SetNumber("columns");
+
+    int[,] matrix = GetMatrix(rows, columns);
+    PrintMatrix(matrix);
+}
+
+int SetNumber(string numberName)
+{
+    Console.Write($"Enter number {numberName}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int[,] GetMatrix(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+
+    var rand = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+
+    return array;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+
+}
