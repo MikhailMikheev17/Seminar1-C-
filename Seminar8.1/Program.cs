d9dd3111e1de9c082a14f64344d298ba2b097b91
MikhailMikheev <Mikhail-Mikheev-7@yandex.ru>
2023-04-17 21:14:33 +0300

    add Seminar8.1

---
+++ Seminar8.1/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+Console.Clear();
+
+Start();
+
+void Start()
+{
+  int rows = SetNumber("rows");
+  int columns = SetNumber("columns");
+
+  int[,] matrix = GetMatrix(rows, columns);
+  PrintMatrix(matrix);
+  System.Console.WriteLine();
+  Reverse(matrix);
+  PrintMatrix(matrix);
+}
+
+int SetNumber(string numberName)
+{
+  Console.Write($"Enter number {numberName}: ");
+  int num = Convert.ToInt32(Console.ReadLine());
+  return num;
+}
+
+int[,] GetMatrix(int rows, int columns)
+{
+  int[,] array = new int[rows, columns];
+
+  var rand = new Random();
+
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < columns; j++)
+    {
+      array[i, j] = i + j;
+    }
+  }
+
+  return array;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      System.Console.Write($"{matrix[i, j]} ");
+    }
+    System.Console.WriteLine();
+  }
+
+}
+
+void Reverse(int[,] matrix)
+{
+  int tmp;
+  for (int j = 0; j < matrix.GetLength(1); j++)
+  {
+    tmp = matrix[0, j];
+    matrix[0, j] = matrix[matrix.GetLength(0) - 1, j];
+    matrix[matrix.GetLength(0) - 1, j] = tmp;
+  }
+}
